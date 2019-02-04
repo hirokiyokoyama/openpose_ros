d39e6a328a6354d16516bb8f5951b4e56ee75ce2
hirokiyokoyama <javawriter.hy@gmail.com>
2019-02-04 16:14:05 +0900

    Update visualize.py

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -7,9 +7,9 @@ from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 from message_filters import ApproximateTimeSynchronizer, TimeSynchronizer, Subscriber
 import numpy as np
-#import labels
 
 bridge = CvBridge()
+image = None
 
 def callback(image_msg, people_msg):
     print 'callback'
@@ -24,18 +24,17 @@ def callback(image_msg, people_msg):
         b = int(np.random.randint(255))
         for part in person.body_parts:
             cv2.circle(cv_image, (int(part.x), int(part.y)), 5, (r, g, b), 2)
-
-    image_message = bridge.cv2_to_imgmsg(cv_image, encoding="passthrough")
-    image_pub.publish(image_message)
-
-    #cv2.imshow('cv_image', cv_image)
-    #cv2.waitKey(1)
+    
+    global image
+    image = cv_image
 
 if __name__ == '__main__':
     rospy.init_node('visualize_born')
-    image_sub = Subscriber('/hsrb/head_rgbd_sensor/rgb/image_rect_color', Image)
-    people_sub = Subscriber('/people', PersonArray)
+    image_sub = Subscriber('image', Image)
+    people_sub = Subscriber('people', PersonArray)
     sub = ApproximateTimeSynchronizer([image_sub, people_sub], 100, 100)
-    image_pub = rospy.Publisher('/bone_image', Image, queue_size=1)
     sub.registerCallback(callback)
-    rospy.spin()
+    while not rospy.is_shutdown():
+        if image is not None:
+            cv2.imshow('OpenPose visualization', image)
+        cv2.waitKey(1)
