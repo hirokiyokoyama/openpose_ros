e02d522c37044c16b79277102dd56add52fef2c9
Hiroki Yokoyama <hiroki.yokoyama@okadanet.org>
2019-04-23 12:42:14 +0900

    fit to python3

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -1,23 +1,23 @@
 #! /usr/bin/env python
 
 import rospy
-from openpose_ros.msg import PersonArray
+from openpose_ros.srv import DetectPeople
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-from message_filters import ApproximateTimeSynchronizer, TimeSynchronizer, Subscriber
 import numpy as np
 from labels import POSE_COCO_L1
 
 bridge = CvBridge()
 image = None
 
-def callback(image_msg, people_msg):
-    print 'callback'
+def callback(image_msg):
     try:
         cv_image = bridge.imgmsg_to_cv2(image_msg, 'bgr8')
     except CvBridgeError as e:
         rospy.logerr(e)
+
+    people_msg = detect_people(image_msg)
     for person in people_msg.people:
         for part in person.body_parts:
             cv2.circle(cv_image, (int(part.x), int(part.y)), 3, (0, 0, 255), 1)
@@ -30,11 +30,9 @@ def callback(image_msg, people_msg):
     image = cv_image
 
 if __name__ == '__main__':
-    rospy.init_node('visualize_born')
-    image_sub = Subscriber('image', Image)
-    people_sub = Subscriber('people', PersonArray)
-    sub = ApproximateTimeSynchronizer([image_sub, people_sub], 100, 100)
-    sub.registerCallback(callback)
+    rospy.init_node('visualize_openpose')
+    detect_people = rospy.ServiceProxy('detect_people', DetectPeople)
+    image_sub = rospy.Subscriber('image', Image, callback)
     while not rospy.is_shutdown():
         if image is not None:
             cv2.imshow('OpenPose visualization', image)
