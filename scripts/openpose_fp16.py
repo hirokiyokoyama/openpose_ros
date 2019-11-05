78dfcfc5aad996901c25910088e8230ae855afcf
Hiroki Yokoyama <h-yokoyama@craft-server.co.jp>
2019-11-05 15:06:08 +0900

    add input_size parameter

--- scripts/openpose_fp16.py
+++ scripts/openpose_fp16.py
@@ -26,7 +26,10 @@ class KeyPointDetector:
                limbs=None, input_size=None):
     self.model = model
     self.ckpt_file = ckpt_file
-    self._input_size = tuple(input_size)
+    if input_size is None:
+      self._input_size = None
+    else:
+      self._input_size = tuple(input_size)
     self.part_names = part_names
     if limbs is not None:
       self.limbs = [(part_names.index(p), part_names.index(q)) for p, q in limbs]
@@ -39,7 +42,7 @@ class KeyPointDetector:
 
   def initialize(self):
     shape = [None,None,None,3]
-    if self.input_shape is not None:
+    if self.input_size is not None:
       shape[1] = self.input_size[0]
       shape[2] = self.input_size[1]
     self.model.build(tuple(shape))
@@ -53,7 +56,7 @@ class KeyPointDetector:
   def _run(self, image,
            key_point_threshold = tf.constant(0.5)):
     image = tf.cast(image[tf.newaxis], tf.float16) / 255.
-    if self.input_size:
+    if self.input_size is not None:
       image = tf.image.resize(image, self.input_size)
       
     heat_map, affinity = self.model(image)
@@ -126,13 +129,21 @@ if __name__ == '__main__':
   bridge = CvBridge()
   rospy.init_node('openpose')
 
+  image_width = rospy.get_param('openpose_image_width', 360)
+  image_height = rospy.get_param('openpose_image_height', 270)
+  if image_width > 0 and image_height > 0:
+    input_size = (image_height, image_width)
+  else:
+    input_size = None
+  
   ckpt_file = os.path.join(pkg, '_data', 'openpose_fp16.ckpt')
   pose_detector = KeyPointDetector(
-    pose_net_body_25(tf.float16), ckpt_file, input_size=(480,640))
+    pose_net_body_25(tf.float16), ckpt_file, input_size=input_size)
   pose_detector.initialize()
   pose_params = {}
 
-  image_sub = rospy.Subscriber('image', Image, callback)
+  image_sub = rospy.Subscriber('image', Image, callback,
+                               queue_size=1, buff_size=1024*1024*4, tcp_nodelay=True)
   people_pub = rospy.Publisher('people', PersonArray, queue_size=1)
   rospy.Service('detect_people', DetectPeople, detect_people)
 
