189a93ba904a64340211404769f5d1cda0872019
Hiroki Yokoyama <h-yokoyama@craft-server.co.jp>
2019-11-05 16:55:59 +0900

    add visualize

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -11,20 +11,56 @@ from labels import POSE_BODY_25_L1
 bridge = CvBridge()
 image = None
 
+limb_colors = [
+  (255,0,255),
+  (255,255,0),
+  (255,128,0),
+  (128,255,0),
+  (0,255,128),
+  (0,255,255),
+  (0,128,255),
+  (128,255,0),
+  (255,255,0),
+  (255,128,0),
+  (255,255,128),
+  (0,255,128),
+  (0,255,255),
+  (0,128,255),
+  (128,255,255),
+  (255,128,255),
+  (255,255,128),
+  (128,255,255),
+  (255,128,128),
+  (128,128,255),
+  (0,0,255),
+  (0,0,255),
+  (0,0,255),
+  (255,0,0),
+  (255,0,0),
+  (255,0,0),
+]
+
+def draw_peoplemsg(image, people):
+  for person in people.people:
+    points = {}
+    for part in person.body_parts:
+      k = part.name
+      v = (int(part.x*image.shape[1]), int(part.y*image.shape[0]))
+      points[k] = v
+      cv2.circle(image, v, 3, (0, 255, 0), 1)
+    for (p1, p2), color in zip(POSE_BODY_25_L1, limb_colors):
+      if p1 in points and p2 in points:
+        cv2.line(image, points[p1], points[p2], color, 3)
+
 def callback(image_msg):
     try:
         cv_image = bridge.imgmsg_to_cv2(image_msg, 'bgr8')
     except CvBridgeError as e:
         rospy.logerr(e)
 
-    people_msg = detect_people(image_msg)
-    for person in people_msg.people:
-        for part in person.body_parts:
-            cv2.circle(cv_image, (int(part.x), int(part.y)), 3, (0, 0, 255), 1)
-        parts = {part.name: (int(part.x), int(part.y)) for part in person.body_parts}
-        for p1, p2 in POSE_BODY_25_L1:
-            if p1 in parts and p2 in parts:
-                cv2.line(cv_image, parts[p1], parts[p2], (0, 255, 0), 2)
+    people_msg = detect_people(cv_image)
+    cv_image = cv_image.copy()
+    draw_peoplemsg(cv_image, people_msg)
     
     global image
     image = cv_image
